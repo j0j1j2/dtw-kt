aff4e2f6fd14ee0a80192669898d557ba034f89b
j0j1j2 <jaeyoung.choi@docenty.ai>
2025-02-15 00:28:24 +0900

    add test

---
+++ src/test/kotlin/DynamicTimeWarpingTest.kt
@@ -0,0 +1,27 @@
+import TimeSeriesDouble
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DynamicTimeWarpingTest {
+    @Test
+    fun testFastWarp() {
+        val a = timeSeriesDoubleListOf(1.0, 2.0, 3.0, 4.0, 5.0)
+        val b = timeSeriesDoubleListOf(2.0, 3.0, 4.0)
+
+        val dtw = DynamicTimeWarping<TimeSeriesDouble>()
+        val (cost, path) = dtw.fastWarp(a, b)
+
+        assertEquals(listOf(Pair(0, 0), Pair(1, 1), Pair(2, 1), Pair(3, 2), Pair(4, 2)), path)
+    }
+
+    @Test
+    fun testWarp() {
+        val dtw = DynamicTimeWarping<TimeSeriesDouble>()
+        val a = timeSeriesDoubleListOf(1.0, 2.0, 3.0, 4.0, 5.0)
+        val b = timeSeriesDoubleListOf(2.0, 3.0, 4.0)
+
+        val (cost, path) = dtw.warp(a, b)
+        assertEquals(2.0, cost)
+        assertEquals(listOf(Pair(0, 0), Pair(1, 0), Pair(2, 1), Pair(3, 2), Pair(4, 2)), path)
+    }
+}
